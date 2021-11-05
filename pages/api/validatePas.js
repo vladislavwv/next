7c7ae8fd36e015a8b3bac07a617c880fab281f95
Vladislav <vlad3220919941919@gmail.com>
2021-11-05 11:27:18 +0200

    new app

--- pages/api/validatePas.js
+++ pages/api/validatePas.js
@@ -22,7 +22,7 @@ export default async (req, res) => {
   
 
     if (strapiRes.ok) {
-      res.status(200).json({ email: data.email })
+      res.status(200).json({ user: data.user })
     } else {
       res
         .status(data.statusCode)
