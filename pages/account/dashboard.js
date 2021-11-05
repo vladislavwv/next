7b08cc500548737a88b7ec0691a77d96fa207f9e
Vladislav <vlad3220919941919@gmail.com>
2021-11-05 15:14:32 +0200

    new app

--- pages/account/dashboard.js
+++ pages/account/dashboard.js
@@ -1,9 +1,9 @@
 //+
-import { parseCookies } from '@/helpers/index'
+import { parseCookies } from '../../helpers/index'
 import { useRouter } from 'next/router'
-import Layout from '@/components/Layout'
+import Layout from '../../components/Layout'
 // import DashboardEvent from '@/components/DashboardEvent'
-import { API_URL } from '@/config/index'
+import { API_URL } from '../../config/index'
 import styles from '@/styles/Dashboard.module.css'
 
 export default function DashboardPage({ events, token }) {
