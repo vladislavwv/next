01d7274d24bb6fbce440422debd5c5f59be4fbcb
Vladislav <vlad3220919941919@gmail.com>
2021-11-08 09:32:46 +0200

    new app

--- pages/account/dashboard.js
+++ pages/account/dashboard.js
@@ -9,37 +9,12 @@ import styles from '@/styles/Dashboard.module.css'
 export default function DashboardPage({ events, token }) {
   const router = useRouter()
 
-  const deleteEvent = async (id) => {
-    if (confirm('Are you sure?')) {
-      const res = await fetch(`${API_URL}/events/${id}`, {
-        method: 'DELETE',
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-
-      const data = await res.json()
-
-      if (!res.ok) {
-        toast.error(data.message)
-      } else {
-        router.reload()
-      }
-    }
-  }
 
   return (
     <Layout title='User Dashboard'>
-      {console.log(parseCookies(req))}
-      <h1>Hello {events.name}</h1>
-      {/* <div className={styles.dash}>
-        <h1>Dashboard</h1>
-        <h3>My Events</h3>
-
-        {events.map((evt) => (
-          <DashboardEvent key={evt.id} evt={evt} handleDelete={deleteEvent} />
-        ))}
-      </div> */}
+      {console.log(token)}
+      <h1>Hello </h1>
+  
     </Layout>
   )
 }
@@ -47,19 +22,10 @@ export default function DashboardPage({ events, token }) {
 export async function getServerSideProps({ req }) {
   const { token } = parseCookies(req)
 
-  const res = await fetch(`${API_URL}/wp-json/wp/v2/users/me`, {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  })
-
-  const events = await res.json()
 
   return {
     props: {
-      events,
-      token,
+      token
     },
   }
 }
