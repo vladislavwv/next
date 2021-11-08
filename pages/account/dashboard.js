a2d8d99deb898d594c354501735cccedb2118417
Vladislav <vlad3220919941919@gmail.com>
2021-11-08 08:50:57 +0200

    new app

--- pages/account/dashboard.js
+++ pages/account/dashboard.js
@@ -10,13 +10,40 @@ export default function DashboardPage({ events, token }) {
   const router = useRouter()
 
 
-
   return (
     <Layout title='User Dashboard'>
-     
-      <h1>Hello </h1>
-    
+      {console.log(events)}
+      <h1>Hello {events.name}</h1>
+      {console.log(events)}
+      {console.log(token)}
+      {/* <div className={styles.dash}>
+        <h1>Dashboard</h1>
+        <h3>My Events</h3>
+
+        {events.map((evt) => (
+          <DashboardEvent key={evt.id} evt={evt} handleDelete={deleteEvent} />
+        ))}
+      </div> */}
     </Layout>
   )
 }
 
+export async function getServerSideProps({ req }) {
+  const { token } = parseCookies(req)
+
+  const res = await fetch(`${API_URL}/wp-json/wp/v2/users/me`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+
+  const events = await res.json()
+
+  return {
+    props: {
+      events,
+      token,
+    },
+  }
+}
