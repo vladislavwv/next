efaf745a1d98232799fb0c58085a5b6b4684319e
Vladislav <vlad3220919941919@gmail.com>
2021-11-08 18:37:37 +0200

    new app

--- pages/api/user.js
+++ pages/api/user.js
@@ -12,7 +12,7 @@ export default async (req, res) => {
     const { token } = cookie.parse(req.headers.cookie)
 
 
-    const strapiRes = await fetch(`${API_URL}/wp-json/jwt-auth/v1/token`, {
+    const strapiRes = await fetch(`${API_URL}/wp-json/wp/v2/users/me`, {
       method: 'GET',
       headers: {
         Authorization: `Bearer ${token}`,
