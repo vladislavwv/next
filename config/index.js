acd2720c6bd30feffffe5541f7b63c246b360d41
Vladislav <vlad3220919941919@gmail.com>
2021-11-08 13:28:27 +0200

    new app

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
-export const API_URL = 'https://my-wordpress-site.hqsite.online/sitetest'
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://my-wordpress-site.hqsite.online/sitetest'
 
-export const NEXT_URL = 'https://next-seven-rho.vercel.app'
+export const NEXT_URL = process.env.NEXT_PUBLIC_API_URL || 'https://next-seven-rho.vercel.app'
 
 export const PER_PAGE = 5
