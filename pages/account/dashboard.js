68479788b73bc112258bfd49c9280fd254a74802
Vladislav <vlad3220919941919@gmail.com>
2021-11-14 11:51:27 +0200

    new app

--- pages/account/dashboard.js
+++ pages/account/dashboard.js
@@ -47,7 +47,7 @@ export default function DashboardPage({ events, token }) {
 export async function getServerSideProps({ req }) {
   const { token } = parseCookies(req)
 
-  const res = await fetch(`${API_URL}/wp-json/wp/v2/users`, {
+  const res = await fetch(`${API_URL}/wp-json/wp/v2/users/me?_${token}`, {
     method: 'GET',
     headers: {
       Authorization: `Bearer ${token}`,
