2cc170cde2e793ef609aed77b8196700c257644e
Vladislav <vlad3220919941919@gmail.com>
2021-11-19 16:27:32 +0200

    new app

--- pages/api/lost.js
+++ pages/api/lost.js
@@ -18,16 +18,15 @@ export default async (req, res) => {
       }),
     })
     const data = await strapiRes.json()
-    console.log(data)
   
 
     if (strapiRes.ok) {
       res.setHeader(
         'Set-Cookie',
-        cookie.serialize('email', data.email, {
+        cookie.serialize('email', email, {
           httpOnly: true,
           secure: process.env.NODE_ENV !== 'development',
-          maxAge: 60 * 60 * 24 * 1, // 1 week
+          maxAge: 60 * 60 * 24 * 7, // 1 week
           sameSite: 'strict',
           path: '/',
         })
