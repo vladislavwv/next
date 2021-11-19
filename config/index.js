24f3b520bb1a4ff54f67877139c18407cc1bb18e
Vladislav <vlad3220919941919@gmail.com>
2021-11-19 14:35:49 +0200

    new app

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://my-wordpress-site.hqsite.online/sitetest'
 
-export const NEXT_URL = 'https://next-word-pi.vercel.app'
+export const NEXT_URL = 'https://next-word.vercel.app'
 
 export const PER_PAGE = 5
